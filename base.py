6fe1ddf39581258d5afdc9775de8c740e5c26543
Patryk Jedlikowski <patryk.jedlikowski@sybillatechnologies.com>
2020-06-10 19:59:21 +0200

    body weight, body height plots

--- base.py
+++ base.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+import os
 
 app=Flask(__name__)
 
@@ -21,15 +22,45 @@ def showing(sur="", all=0):
             temp['Id']=str(x['resource']['identifier'][0]['value'])
     return PatientData
 
-def showingObservation(s, sur="", all=0, minDate="1928-04-28", maxDate="2112-04-28"):
-    r = requests.get(url = f"http://localhost:8090/baseDstu3/Observation?patient.identifier={s}", params = "")
+def showingObservation(s, sur="", all=0, minDate="1928-04-28", maxDate="2112-04-28", minDateHeight="1928-04-28", maxDateHeight="2112-04-28", minDateWeight="1928-04-28", maxDateWeight="2112-04-28"):
+    r = requests.get(url = f"http://localhost:8090/baseDstu3/Observation?patient.identifier={s}&_count=1000000", params = "")
     minDateTime=time.strptime(minDate, "%Y-%m-%d")
     maxDateTime=time.strptime(maxDate, "%Y-%m-%d")
+    minDateTimeHeight=time.strptime(minDateHeight, "%Y-%m-%d")
+    maxDateTimeHeight=time.strptime(maxDateHeight, "%Y-%m-%d")
+    minDateTimeWeight=time.strptime(minDateWeight, "%Y-%m-%d")
+    maxDateTimeWeight=time.strptime(maxDateWeight, "%Y-%m-%d")
+
+    # body height creator
+    plotData_height = [["date","close"]]
+    for x in r.json()['entry']:
+        Observ_date_string = str(x['resource']['effectiveDateTime'])
+        Observ_date=time.strptime(Observ_date_string[:10], "%Y-%m-%d")
+        display = str(x['resource']['code']['text'])
+        if (maxDateTimeHeight < Observ_date or Observ_date < minDateTimeHeight):
+            continue
+        if(display == "Body Height"):
+            plotData_height.append([ Observ_date_string[:10], str(x['resource']['valueQuantity']['value']) ])
+    with open('data_height.csv','w') as f:
+        f.write('\n'.join([','.join(x) for x in plotData_height]))
+
+    # body weight creator
+    plotData_weight = [["date","close"]]
+    for x in r.json()['entry']:
+        Observ_date_string = str(x['resource']['effectiveDateTime'])
+        Observ_date=time.strptime(Observ_date_string[:10], "%Y-%m-%d")
+        display = str(x['resource']['code']['text'])
+        if (maxDateTimeWeight < Observ_date or Observ_date < minDateTimeWeight):
+            continue
+        if(display == "Body Weight"):
+            plotData_weight.append([ Observ_date_string[:10], str(x['resource']['valueQuantity']['value']) ])
+    with open('data_weight.csv','w') as f:
+        f.write('\n'.join([','.join(x) for x in plotData_weight]))
 
     PatientData=[]
     for x in r.json()['entry']:
         #if ()
-        if (True or all==1 or re.search(sur, str(x['resource']['name'][0]['family'])) or re.search(sur, str(x['resource']['name'][0]['given']))):
+        if (True or all==1 or re.search(sur, str(x['resource']['code']['text']))):
             Observ_date=time.strptime(str(x['resource']['effectiveDateTime'])[:10], "%Y-%m-%d")
             if (maxDateTime < Observ_date or Observ_date < minDateTime):
                 continue
@@ -44,10 +75,28 @@ def showingObservation(s, sur="", all=0, minDate="1928-04-28", maxDate="2112-04-
             #     for comp in x['resource']['component']:
             #         temp['Component']['Text'].append(comp['code']['text'])
             #         temp['Component']['Value'].append(f"{comp['valueQuantity']['value']} [{comp['valueQuantity']['code']}]")
+            
+            
+            
+            
+            
+
             try:
                 temp['Value']=f"{x['resource']['valueQuantity']['value']} [{x['resource']['valueQuantity']['code']}]" 
             except:
-                if temp['Display'] == "Tobacco smoking status NHIS":
+                if temp['Display'] in ["Tobacco smoking status NHIS",
+                                         "Appearance of Urine", 
+                                         "Odor of Urine", 
+                                         "Clarity of Urine", 
+                                         "Color of Urine",
+                                          "Glucose [Presence] in Urine by Test strip",
+                                          "Bilirubin.total [Presence] in Urine by Test strip",
+                                          "Ketones [Presence] in Urine by Test strip",
+                                          "Protein [Presence] in Urine by Test strip",
+                                          "Nitrite [Presence] in Urine by Test strip",
+                                          "Hemoglobin [Presence] in Urine by Test strip",
+                                          "Leukocyte esterase [Presence] in Urine by Test strip"
+                                          ]:
                     # zisDict=x['resource']['component']
                     # temp['Display']=zisDict[0]['code']['text']
                     # temp['Value']=f"{zisDict[0]['valueQuantity']['value']} [{zisDict[0]['valueQuantity']['code']}]"
@@ -58,6 +107,7 @@ def showingObservation(s, sur="", all=0, minDate="1928-04-28", maxDate="2112-04-
                     # PatientData.append(temp2)
                     temp['Value']=""
                 else:
+                    print(temp['Display'])
                     temp['Value']=""
                     temp['Component'] = {}   
                     temp['Component']['Text'] = list()
@@ -65,30 +115,36 @@ def showingObservation(s, sur="", all=0, minDate="1928-04-28", maxDate="2112-04-
                     for comp in x['resource']['component']:
                         temp['Component']['Text'].append(comp['code']['text'])
                         temp['Component']['Value'].append(f"{comp['valueQuantity']['value']} [{comp['valueQuantity']['code']}]") 
+    
     return PatientData
 
 
 def showingMedicationR(s, sur="", all=0, minDate="1928-04-28", maxDate="2112-04-28"):
-    r = requests.get(url = f"http://localhost:8090/baseDstu3/MedicationRequest?patient.identifier={s}", params = "")
+    r = requests.get(url = f"http://localhost:8090/baseDstu3/MedicationRequest?patient.identifier={s}&_count=1000000", params = "")
     minDateTime=time.strptime(minDate, "%Y-%m-%d")
     maxDateTime=time.strptime(maxDate, "%Y-%m-%d")
 
-
-
     PatientData=[]
+    if not 'entry' in r.json():
+        return
     for x in r.json()['entry']:
         if (True or all==1 or re.search(sur, str(x['resource']['name'][0]['family'])) or re.search(sur, str(x['resource']['name'][0]['given']))):
             Observ_date=time.strptime(str(x['resource']['authoredOn'])[:10], "%Y-%m-%d")
-            if (maxDateTime <= Observ_date or Observ_date <= minDateTime):
+            if (maxDateTime < Observ_date or Observ_date < minDateTime):
                 continue
 
             temp={}
             PatientData.append(temp)
             temp['Date']=str(x['resource']['authoredOn'])
             temp['Status']=str(x['resource']['status'])
-            temp['Medication']=f"{x['resource']['medicationCodeableConcept']['coding'][0]['text']}"
+            temp['Medication']=f"{x['resource']['medicationCodeableConcept']['text']}"
             try:
-                temp['dosageInstruction']=f"{x['resource']['dosageInstruction']['additionalInstruction']['text']}" 
+                dct = x['resource']['dosageInstruction'][0]['timing']['repeat']
+                temp['Timing']=f"frequency: {dct['frequency']}  period: {dct['period']}  periodUnit: {dct['periodUnit']}" 
+            except:
+                temp['Timing']=""
+            try:
+                temp['dosageInstruction']=f"{x['resource']['dosageInstruction'][0]['additionalInstruction'][0]['text']}" 
             except:
                 temp['dosageInstruction']=""
 
@@ -97,20 +153,39 @@ def showingMedicationR(s, sur="", all=0, minDate="1928-04-28", maxDate="2112-04-
 
 #href=f"http://localhost:8090/baseDstu3/Observation?patient.identifier={y[x]}"
 def createElem(doc, tab, elemList, namez=['Name', 'Surname', 'Id']):
-    print(tab)
+    currentClass="0"
+    if not elemList:
+        return
     for y in elemList:
         trow = doc.new_tag("tr")
+        trow['class']=currentClass
         tab.append(trow)
         for x in namez:
-            info=doc.new_tag("td")
-            if (x=='Id'):
-                #link=doc.new_tag("button", form="page", formmethod="post", type="submit", name="next", value=y[x])
-                link=doc.new_tag("button", attrs={"form":"page", "formmethod":"post", "type":"submit", "name":"next", "value":y[x]})
-                link.string=y[x]
-                info.append(link)
-            else:
-                info.string=y[x]
-            trow.append(info)
+            if ('Component' in y and x=='Component'):
+                for t, v in zip(y[x]['Text'], y[x]['Value']):
+                    trow_inner = doc.new_tag("tr")
+                    info1=doc.new_tag("td")
+                    info_text=doc.new_tag("td")
+                    info_text.string=t
+
+                    info_value=doc.new_tag("td")
+                    info_value.string=v
+                    _=[trow_inner.append(x) for x in [info1, info_text, info_value]]
+
+                    trow_inner['class']=currentClass
+                    tab.append(trow_inner)
+
+            elif (x!='Component'):
+                info=doc.new_tag("td")
+                if (x=='Id'):
+                    #link=doc.new_tag("button", form="page", formmethod="post", type="submit", name="next", value=y[x])
+                    link=doc.new_tag("button", attrs={"form":"page", "formmethod":"post", "type":"submit", "name":"next", "value":y[x]})
+                    link.string=y[x]
+                    info.append(link)
+                else:
+                    info.string=y[x]
+                trow.append(info)
+        currentClass=str(int(currentClass)+1)
 
 @app.route('/', methods=['GET', 'POST'])
 def Wisdom():
@@ -120,26 +195,48 @@ def Wisdom():
     global patientId
 
     data=souper.find("tbody", id="Patients")
+    med_data=souper.find("tbody", id="Medication")
     if (req.method=='POST'):
         s=req.form['next']
         if (s=='search'):
             jsonInfo=showing(sur=req.form['Surname'])
             createElem(souper, data, jsonInfo)
+
+
         elif (s=='searchTime'):
-            jsonInfo=showingObservation(patientId, minDate=req.form['MinDate'], maxDate=req.form['MaxDate'])
-            print(jsonInfo)
+            jsonInfo=showingObservation(patientId, minDate=req.form['MinDate'], 
+                                                   maxDate=req.form['MaxDate'],
+                                                   minDateHeight=req.form['MinDateHeight'], 
+                                                   maxDateHeight=req.form['MaxDateHeight'], 
+                                                   minDateWeight=req.form['MinDateWeight'], 
+                                                   maxDateWeight=req.form['MaxDateWeight'])
+
+            jsonInfo_medic=showingMedicationR(patientId, minDate=req.form['MinDate'], maxDate=req.form['MaxDate'])
+
             data=souper.find("tbody", id="Observation")
-            createElem(souper, data, jsonInfo, ['Display', 'Date', 'Value'])
+            createElem(souper, data, jsonInfo, ['Date', 'Display', 'Value'])
 
             souper.find("input", id="MinDate")["value"]=req.form['MinDate']
             souper.find("input", id="MaxDate")["value"]=req.form['MaxDate']
+            souper.find("input", id="MinDateHeight")["value"]=req.form['MinDateHeight']
+            souper.find("input", id="MaxDateHeight")["value"]=req.form['MaxDateHeight']
+            souper.find("input", id="MinDateWeight")["value"]=req.form['MinDateWeight']
+            souper.find("input", id="MaxDateWeight")["value"]=req.form['MaxDateWeight']
 
         else:
-            jsonInfo=showingObservation(s, minDate=req.form['MinDate'], maxDate=req.form['MaxDate'])
-            print(jsonInfo)
+            jsonInfo=showingObservation(s, minDate=req.form['MinDate'],
+                                           maxDate=req.form['MaxDate'],
+                                           minDateHeight=req.form['MinDateHeight'], 
+                                           maxDateHeight=req.form['MaxDateHeight'], 
+                                           minDateWeight=req.form['MinDateWeight'], 
+                                           maxDateWeight=req.form['MaxDateWeight'])
+
+            jsonInfo_medic=showingMedicationR(s, minDate=req.form['MinDate'], maxDate=req.form['MaxDate'])
+
             patientId=s
             data=souper.find("tbody", id="Observation")
-            createElem(souper, data, jsonInfo, ['Display', 'Date', 'Value'])
+            createElem(souper, data, jsonInfo, ['Date', 'Display', 'Value', 'Component'])
+            createElem(souper, med_data, jsonInfo_medic, ['Date', 'Status', 'Medication', 'Timing', 'dosageInstruction'])
             #data.string=f"http://localhost:8090/baseDstu3/Observation?patient.identifier={s}"
     
         return render_template_string(souper.prettify())
@@ -147,8 +244,28 @@ def Wisdom():
     else:
         jsonInfo=showing(all=1)
         createElem(souper, data, jsonInfo)
-
+        try:
+            os.remove("data_weight.csv")
+        except:
+            pass
+        try:
+            os.remove("data_height.csv")
+        except:
+            pass
         return render_template_string(souper.prettify())
+
+
+@app.route('/data_height.csv', methods=['GET'])
+def data():
+    html_doc=open('./data_height.csv')
+    souper=BeautifulSoup(html_doc, 'html.parser')
+    return render_template_string(souper.prettify())
+
+@app.route('/data_weight.csv', methods=['GET'])
+def data_weight():
+    html_doc=open('./data_weight.csv')
+    souper=BeautifulSoup(html_doc, 'html.parser')
+    return render_template_string(souper.prettify())
 
 
 if __name__=='__main__':
